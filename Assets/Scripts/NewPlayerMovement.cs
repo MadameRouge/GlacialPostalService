ec46093796d44f8fcfb0026a9c13f861a37afec7
MadameRouge <bh98cx@student.sunderland.ac.uk>
2023-01-07 23:06:52 +0000

    FinalT1

--- Assets/Scripts/NewPlayerMovement.cs
+++ Assets/Scripts/NewPlayerMovement.cs
@@ -10,12 +10,15 @@ public class NewPlayerMovement : MonoBehaviour
     Rigidbody rb;
 
     float speed = 6.0f;
-    public float jumpSpeed = 1f;
+    public float jumpSpeed = 100f;
     public float gravity = 5.0f;
     public GameObject playermodel;
+    public GameObject hitTarget;
 
     private Vector3 moveDirection = Vector3.zero;
     private Vector3 LookatDirection = Vector3.zero;
+
+    private int CheckLayers;
 
     void Start()
     {
@@ -26,34 +29,74 @@ public class NewPlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Using Checklayers you need to check each time the player moves what type of floor they are on. Then inside a function lets say. Have an if statement for each one changing your speed.
         if (Input.GetKey(KeyCode.W))
         {
-            //x, y, z
-            rb.velocity = new Vector3(-speed, 0, 0);
+            //groundCheck();
+            rb.velocity = new Vector3(-speed, rb.velocity.y, 0);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            //x, y, z
-            rb.velocity = new Vector3(speed, 0, 0);
+            //groundCheck();
+            rb.velocity = new Vector3(speed, rb.velocity.y, 0);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            //x, y, z
-            rb.velocity = new Vector3(0, 0, -speed);
+            //groundCheck();
+            rb.velocity = new Vector3(0, rb.velocity.y, -speed);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            //x, y, z
-            rb.velocity = new Vector3(0, 0, speed);
+            //groundCheck();
+            rb.velocity = new Vector3(0, rb.velocity.y, speed);
         }
 
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            //x, y, z
-            rb.velocity = new Vector3(0, jumpSpeed, 0);
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        //CheckLayers is checking for layer 15 (Water) and then inverting it so that the raycast looks for everything except layer 15.
+        CheckLayers = 1 << 15;
+        CheckLayers = ~CheckLayers;
+        RaycastHit hit;
+        
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 1f, CheckLayers))
+        {
+                rb.velocity = new Vector3(rb.velocity.x, jumpSpeed, rb.velocity.z);
+        }
+
+        Debug.Log(CheckLayers);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Ice
+        if (collision.gameObject.layer == 13)
+        {
+            speed = 5.0f;
+            rb.drag = 0.5f;
+            Debug.Log("Ice Detected");
+        }
+        //Snow
+        else if (collision.gameObject.layer == 14)
+        {
+            speed = 5.0f;
+            rb.drag = 1.0f;
+            Debug.Log("Snow Detected");
+        }
+        //Grass
+        else if (collision.gameObject.layer == 16)
+        {
+            speed = 5.0f;
+            rb.drag = 0.25f;
+            Debug.Log("Grass Detected");
         }
     }
 }
